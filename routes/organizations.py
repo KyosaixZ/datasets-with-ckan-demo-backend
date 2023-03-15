23e39fd9b6a400689e1212fe049b34e16c50dac6
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2023-03-16 02:50:10 +0700

    enhancement: organizations api

--- routes/organizations.py
+++ routes/organizations.py
@@ -4,7 +4,16 @@ from ckan.ckan_connect import ckan_connect
 organizations_route = Blueprint('organizations_route', __name__)
 
 # get all organization name
-@organizations_route.route('/', methods=['GET'])
+@organizations_route.route('/name', methods=['GET'])
 def get_organizations_name():
 	with ckan_connect() as ckan:
 		return ckan.action.organization_list()
+
+# get all organization, include details
+@organizations_route.route('/', methods=['GET'])
+def get_organizations():
+	order = request.args.get('order_by')
+	if order is None:
+		order = 'name'
+	with ckan_connect() as ckan:
+		return ckan.action.organization_list(all_fields=True, order_by=order)
