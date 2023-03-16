157b20dfb87ca723c2039cb1e8648b9705ef9f6c
JcsnP <jcsnpch@gmail.com>
2023-03-17 03:17:44 +0700

    get user details

--- postgresql/User.py
+++ postgresql/User.py
@@ -2,6 +2,9 @@ import os
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 from passlib.hash import pbkdf2_sha512
+import jwt
+# load env file
+load_dotenv()
 
 from core.PostgreSQL import PostgreSQL
 
@@ -18,8 +21,9 @@ class User(PostgreSQL):
 	state: str
 	image: str
 	last_active: str
+	secret: os.getenv('SECRET')
 
-	def __init__(self):
+	def __init__(self, api_key:str = None):
 		super().__init__()
 
 	def _verify_password(self, password, pasword_hashed):
@@ -27,6 +31,10 @@ class User(PostgreSQL):
 			return True
 		else:
 			return False
+		
+	def _verify_token(self, token:str = None):
+		# if success set the user id
+		
 
 	def login(self, name, password):
 		with self.engine.connect() as connection:
@@ -34,13 +42,29 @@ class User(PostgreSQL):
 			# query the user
 			result = connection.execute(text(query_string)).one()
 			# then check a name and hashed passwors
+			# result[0] = id
 			# result[1] = name
 			# result[5] = password
 
 			if result[1] == name and self._verify_password(password, result[5]):
-				return True
+				# set user details
+				self.id = result[0]
+				self.name = result[1]
+				# generate a jwt token or something
+				# then, return token to user
+				return token
 			else:
 				return False
+
+	def get_user_details(self, token):
+		if self._verify_token(token):
+			# if verify success
+			with self.engine.connect() as connection:
+				query_string = "SELECT id, name, apikey, created, about, password, fullname, email, sysadmin, activity_streams_email_notifications, stat, image_url, last_active FROM public.user WHERE id = '%s'" % self.id
+				# query the user details
+				result = connection.execute(text(query_string))
+				if result is not None:
+					return result
 
 taworn = User()
 print(taworn.login('taworn', 'test1235'))
